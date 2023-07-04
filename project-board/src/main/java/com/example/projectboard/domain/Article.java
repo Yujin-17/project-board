cced6022586bab4b414f5255ccb1abaa80513f7a
yujin-17 <lyjyellow@gmail.com>
2023-07-04 23:34:17 +0900

    Feat: application.yaml 파일 정의

--- project-board/src/main/java/com/example/projectboard/domain/Article.java
+++ project-board/src/main/java/com/example/projectboard/domain/Article.java
@@ -1,7 +1,13 @@
 package com.example.projectboard.domain;
 
 import java.time.LocalDateTime;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.ToString;
 
+@Getter // 모든 field 는 접근 가능해야한다. / @Setter 도 추가할 수 있지만, 여기서는 사용 안하는 방향으로 함.
+@ToString // 쉽게 출력 가능 -> 쉬운 관찰
+@Table(indexs = ) // index 생성 가능.
 public class Article {
 
   // 이 도메인에 관련된 내용
